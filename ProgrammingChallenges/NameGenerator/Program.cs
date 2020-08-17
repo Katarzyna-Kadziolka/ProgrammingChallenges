bde65edd8908d25ea42c1e57f4e1f74648f8367f
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2020-08-17 13:24:27 +0200

    Class Generator

--- ProgrammingChallenges/NameGenerator/Program.cs
+++ ProgrammingChallenges/NameGenerator/Program.cs
@@ -24,44 +24,5 @@ namespace NameGenerator {
         }
     }
 
-    class Generator {
-        public static string GenerateName(int wordLength) {
-            StringBuilder sb = new StringBuilder();
-            string consonants = "bcdfghjklmnpqrstvwxz";
-            string vowel = "aeiouy";
-
-            Random random = new Random();
-            int start = random.Next(0, 2);
-            for (int i = 0; i < wordLength; i++) {
-                
-                if (start == 0) {
-                    if (i % 2 == 1) {
-                        sb.Append(random.GetRandomChar(consonants));
-                        if (random.Next(0, 3) == 0) {
-                            sb.Append(random.GetRandomChar(consonants));
-                        }
-                    }
-                    else {
-                        sb.Append((random.GetRandomChar(vowel)));
-                    }
-                }
-                else {
-                    if (i % 2 == 1) {
-                        sb.Append(random.GetRandomChar(consonants));
-                        if (random.Next(0, 3) == 0) {
-                            sb.Append(random.GetRandomChar(consonants));
-                        }
-                    }
-                    else {
-                        sb.Append(random.GetRandomChar(vowel));
-                    }
-                }
-                if (sb.Length > wordLength) {
-                    sb.Remove(sb.Length - (sb.Length - wordLength), sb.Length - wordLength);
-                    break;
-                }
-            }
-            return sb.ToString().Capitalize();
-        }
-    }
+    
 }
