1af7b2e88b3bc8d84bc3ff6a0293869467e0bf3c
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2020-09-17 19:01:52 +0200

    CollatzConjecture

---
+++ ProgrammingChallenges/CollatzConjecture/CollatzGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollatzConjecture
+{
+    class CollatzGenerator
+    {
+        public static List<long> CreateCollatzSequece(int startingNumber){
+            List<long> collatzSequence = new List<long> ();
+            CreateCollatz(collatzSequence, startingNumber);
+            return collatzSequence;
+            
+        }
+        private static void CreateCollatz (List<long> collatzSequence, long number) {
+            collatzSequence.Add(number);
+            if (number == 1) {
+                return;
+            }
+            if (number % 2 == 0) {
+                CreateCollatz (collatzSequence, number/2);
+            }
+            else {
+                CreateCollatz (collatzSequence, number * 3 + 1);
+            }
+
+        }
+        
+    }
+}
