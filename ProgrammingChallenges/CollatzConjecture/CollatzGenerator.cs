3957eab69f1c2eea666bbe6c5a0d5a957fb35760
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2020-09-17 20:00:10 +0200

    CollatzConjecture

--- ProgrammingChallenges/CollatzConjecture/CollatzGenerator.cs
+++ ProgrammingChallenges/CollatzConjecture/CollatzGenerator.cs
@@ -2,29 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CollatzConjecture
-{
-    class CollatzGenerator
-    {
-        public static List<long> CreateCollatzSequece(int startingNumber){
-            List<long> collatzSequence = new List<long> ();
+namespace CollatzConjecture {
+    class CollatzGenerator {
+        public static List<long> CreateCollatzSequece(int startingNumber) {
+            List<long> collatzSequence = new List<long>();
             CreateCollatz(collatzSequence, startingNumber);
             return collatzSequence;
-            
         }
-        private static void CreateCollatz (List<long> collatzSequence, long number) {
+
+        private static void CreateCollatz(List<long> collatzSequence, long number) {
             collatzSequence.Add(number);
             if (number == 1) {
                 return;
             }
+
             if (number % 2 == 0) {
-                CreateCollatz (collatzSequence, number/2);
+                CreateCollatz(collatzSequence, number / 2);
             }
             else {
-                CreateCollatz (collatzSequence, number * 3 + 1);
+                CreateCollatz(collatzSequence, number * 3 + 1);
             }
-
         }
-        
     }
 }
--- ProgrammingChallenges/CollatzConjecture/Program.cs
+++ ProgrammingChallenges/CollatzConjecture/Program.cs
@@ -2,35 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace CollatzConjecture
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace CollatzConjecture {
+    class Program {
+        static void Main(string[] args) {
             Console.WriteLine("Welcome in the Collatz Sequence Generator!");
             int startNumber = 0;
             do {
                 Console.WriteLine("Please, enter the starting number (it should be bigger than 1): ");
-                do {
-                    try {startNumber = Convert.ToInt32(Console.ReadLine()); }
-                    catch (System.FormatException) {
-                        Console.WriteLine("It's not a number!");
-                    }
-                } while (false);
-                
+                try {
+                    startNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (System.FormatException) {
+                    Console.WriteLine("It's not a number!");
+                }
             } while (startNumber <= 0);
+
             List<long> collatzSequence = CollatzGenerator.CreateCollatzSequece(startNumber);
-            List<string> collatzSequenceString = new List<string>();
-            for (int i = 0; i < collatzSequence.Count; i++) {
-                collatzSequenceString.Add(collatzSequence[i].ToString());
-            }
-            for (int i = 0; i < collatzSequenceString.Count - 1; i++) {
-                collatzSequenceString[i] = collatzSequenceString[i] + ", ";
-            }
+            
             Console.WriteLine("Your Collatz Sequence: ");
-            collatzSequenceString.ForEach(Console.Write);
+            collatzSequence.ForEach(l => Console.Write(l + "; "));
         }
-        
     }
 }
